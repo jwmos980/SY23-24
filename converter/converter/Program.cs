b4a33166f8809c01d604de3cae66d9e75a182aa2
jackson <jwmos980@learner.dvusd.org>
2024-01-10 09:21:44 -0700

    decoder project

--- converter/converter/Program.cs
+++ converter/converter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,18 +11,84 @@ namespace converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a number");
+            Console.WriteLine("enter the number of liter");
+            int liters = 0;
+            int.TryParse(Console.ReadLine(), out liters);
+
+            Console.WriteLine("press 1 for fluid ounces\npress 2 for gills\npress 3 for pints\npress 4 for quarts\npress 5 for gallons\npress 6 for tablespoon\npress 7 for teaspoon\npress 8 for cup");
             int num = 0;
             int.TryParse(Console.ReadLine(), out num);
-            Console.WriteLine(num * 2);
+
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine(liters * 33.814 + " fl oz");
+                    break;
+                case 2:
+                    Console.WriteLine(liters * 8.45351 + " gi");
+                    break;
+                case 3:
+                    Console.WriteLine(liters * 2.11337 + " pt");
+                    break;
+                case 4:
+                    Console.WriteLine(liters * 1.05668 + " qt");
+                    break;
+                case 5:
+                    Console.WriteLine(liters * 0.26417 + " gal");
+                    break;
+                case 6:
+                    Console.WriteLine(liters * 67.628 + " tbsp");
+                    break;
+                case 7:
+                    Console.WriteLine(liters * 1202.884 + " tsp");
+                    break;
+                case 8:
+                    Console.WriteLine(liters * 4.22675 + " cups");
+                    break;
+                default:
+                    Console.WriteLine("invalid units");
+                    break;
+            }
             Console.ReadLine();
 
+
+
+
             /*
-            for (int i = 0; i < 100; i++)
+            Console.WriteLine("enter the number of meters");
+            int meters = 0;
+            int.TryParse(Console.ReadLine(), out meters);
+
+            Console.WriteLine("press 1 for feet\npress 2 for yards");
+            int num = 0;
+            int.TryParse(Console.ReadLine(), out num);
+
+            
+            if (num == 1)
             {
-                Console.Write(i);
+                Console.WriteLine(meters * 3.28084 + " feet");
             }
-            Console.ReadKey();
+            if (num == 2)
+            {
+                Console.WriteLine(meters * 1.09361 + " yards");
+            }
+            Console.ReadLine();
+            
+
+            switch (num)
+            {
+                case 1: 
+                    Console.WriteLine(meters * 3.28084 + " feet"); 
+                    break;
+                case 2:
+                    Console.WriteLine(meters * 1.09361 + " yards");
+                    break;
+                default:
+                    Console.WriteLine("invalid units");
+                    break;
+            }
+            Console.ReadLine();
             */
         }
     }
---
+++ decoder/decoder/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 'a':
+                    label1.Text += "1";
+                    break;
+                case 'b':
+                    label1.Text += "]";
+                    break;
+                case 'c':
+                    label1.Text += "@";
+                    break;
+                case 'd':
+                    label1.Text += "&";
+                    break;
+                case 'e':
+                    label1.Text += ",";
+                    break;
+                case 'f':
+                    label1.Text += ">";
+                    break;
+                case 'g':
+                    label1.Text += "w";
+                    break;
+                case 'h':
+                    label1.Text += "|";
+                    break;
+                case 'i':
+                    label1.Text += "n";
+                    break;
+                case 'j':
+                    label1.Text += "~";
+                    break;
+                case 'k':
+                    label1.Text += "9";
+                    break;
+                case 'l':
+                    label1.Text += "(";
+                    break;
+                case 'm':
+                    label1.Text += "r";
+                    break;
+                case 'n':
+                    label1.Text += "*";
+                    break;
+                case 'o':
+                    label1.Text += "=";
+                    break;
+                case 'p':
+                    label1.Text += "_";
+                    break;
+                case 'q':
+                    label1.Text += "%";
+                    break;
+                case 'r':
+                    label1.Text += "$";
+                    break;
+                case 's':
+                    label1.Text += "#";
+                    break;
+                case 't':
+                    label1.Text += "!";
+                    break;
+                case 'u':
+                    label1.Text += "L";
+                    break;
+                case 'v':
+                    label1.Text += "d";
+                    break;
+                case 'w':
+                    label1.Text += ")";
+                    break;
+                case 'x':
+                    label1.Text += "^";
+                    break;
+                case 'y':
+                    label1.Text += ".";
+                    break;
+                case 'z':
+                    label1.Text += "-";
+                    break;
+                default:
+                 
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = String.Empty;
+            label2.Text = String.Empty;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case '1':
+                    label2.Text += "a";
+                    break;
+                case ']':
+                    label2.Text += "b";
+                    break;
+                case '@':
+                    label2.Text += "c";
+                    break;
+                case '&':
+                    label2.Text += "d";
+                    break;
+                case ',':
+                    label2.Text += "e";
+                    break;
+                case '>':
+                    label2.Text += "f";
+                    break;
+                case 'w':
+                    label2.Text += "g";
+                    break;
+                case '|':
+                    label2.Text += "h";
+                    break;
+                case 'n':
+                    label2.Text += "i";
+                    break;
+                case '~':
+                    label2.Text += "j";
+                    break;
+                case '9':
+                    label2.Text += "k";
+                    break;
+                case '(':
+                    label2.Text += "l";
+                    break;
+                case 'r':
+                    label2.Text += "m";
+                    break;
+                case '*':
+                    label2.Text += "n";
+                    break;
+                case '=':
+                    label2.Text += "o";
+                    break;
+                case '_':
+                    label2.Text += "p";
+                    break;
+                case '%':
+                    label2.Text += "q";
+                    break;
+                case '$':
+                    label2.Text += "r";
+                    break;
+                case '#':
+                    label2.Text += "s";
+                    break;
+                case '!':
+                    label2.Text += "t";
+                    break;
+                case 'L':
+                    label2.Text += "u";
+                    break;
+                case 'd':
+                    label2.Text += "v";
+                    break;
+                case ')':
+                    label2.Text += "w";
+                    break;
+                case '^':
+                    label2.Text += "x";
+                    break;
+                case '.':
+                    label2.Text += "y";
+                    break;
+                case '-':
+                    label2.Text += "z";
+                    break;
+                default:
+
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
