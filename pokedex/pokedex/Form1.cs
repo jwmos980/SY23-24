feace8c0d3fe5e5c366dcad609b1ece201753b9e
jackson <jwmos980@learner.dvusd.org>
2024-01-22 09:27:37 -0700

    pokemon updated

--- pokedex/pokedex/Form1.cs
+++ pokedex/pokedex/Form1.cs
@@ -13,19 +13,19 @@ namespace pokedex
 {
     enum attack
     {
-        special_attack, special_defense 
+        special_attack, special_defense
     }
     struct pokemon
     {
-        string name;
-        string type;
-        int level;
-        attack attacktype;
-        int hp;
-        int xp;
-        bool legendary;
-        bool shiny;
-        int generation;
+        public string name;
+        public string type;
+        public int level;
+        public attack attacktype;
+        public int hp;
+        public int xp;
+        public bool legendary;
+        public bool shiny;
+        public int generation;
     }
     public partial class Form1 : Form
     {
@@ -40,8 +40,30 @@ namespace pokedex
             {
             StreamReader inFile = new StreamReader("pokemon.txt");
                 string s = inFile.ReadToEnd();
+                readpokemon(s);
                 inFile.Close();
             }
+        }
+        private void readpokemon(string s)
+        {
+            pokemon p = new pokemon();
+            string[] fields = s.Split('|');
+            p.name = fields[0];
+            p.type = fields[1];
+            p.level = int.Parse(fields[2]);
+            p.attacktype = (attack)Enum.Parse(typeof(attack), fields[3]);
+            p.hp = int.Parse(fields[4]);
+            p.xp = int.Parse(fields[5]);
+            if (fields[6] == "true")
+                p.legendary = true;
+            else
+                p.legendary = false;
+            if (fields[7] == "true")
+                p.shiny = true;
+            else
+                p.shiny = false;
+            p.generation = int.Parse(fields[8]);
+
         }
 
         private void savebutton_Click(object sender, EventArgs e)
